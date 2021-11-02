b6db8ffe7fab146a10acfb59d6c36b4c21db3054
taeva <qw01w@yandex.ru>
2021-11-02 18:26:25 +0300

    first

---
+++ cmake-build-debug/CMakeFiles/Login_in.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Login_in.dir/main.cpp.o"
+  "Login_in"
+  "Login_in.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Login_in.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdlib>
+int main() {
+    int true_password = 1;
+    int password;
+    char name [7];
+    char true_name [7] = "Dmitry";
+
+    std::cout << "Perform authorization\nEnter your name:\t\t";
+    std::cin >> name;
+    std::cout << "Enter your password:\t";
+    std::cin >> password;
+    if (name == true_name or true_password == password)
+    {
+        std::cout << "The user's data is correct";
+
+    }
+    else
+    {
+          std::cout << "The user's data is not correct";
+          exit(0);
+    }
+}
